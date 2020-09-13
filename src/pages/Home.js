b61cef62c0a77d302579e34ae15f4e0194610f4c
shhrrtnvr <shhrrtnvr@gmail.com>
2020-09-14 04:27:59 +0600

    Ready to share

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,7 +12,7 @@ function Home() {
             <div className='home_header'>
                 <div className='home_headerLeft'>
                     <Link to='/about'>About</Link>
-                    <Link to='/stor'>Store</Link>
+                    <Link to='/store'>Store</Link>
                 </div>
                 <div className='home_headerRight'>
                     <Link to='gmail'>Gmail</Link>
@@ -25,6 +25,12 @@ function Home() {
             <div className='home_body'>
                 <img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" alt="" />
                 <Search />
+            </div>
+            <div className='home_footer'>
+                <p className='home_footerSignature'>
+                   &copy; Shahriar Tanvir
+                </p>
+
             </div>
 
         </div>
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -3,13 +3,15 @@ import './SearchPage.css';
 import { useStateValue } from '../StateProvider';
 import useGoogleSearch from '../useGoogleSearch';
 import Response from '../response';
-import { Link } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 import Search from '../components/Search';
 import SearchIcon from '@material-ui/icons/Search';
 import ImageIcon from '@material-ui/icons/ImageSearch';
 import NewsIcon from '@material-ui/icons/Description';
 import MapIcon from '@material-ui/icons/RoomOutlined';
 import MoreIcon from '@material-ui/icons/MoreVertRounded';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+                                
 
 function SearchPage() {
     const [{ term }, dispatch] = useStateValue();
@@ -18,10 +20,11 @@ function SearchPage() {
     //MOCK API
     //const data = Response;
 
+    console.log(data);
     return (
         <div className="SearchPage">
             <div className="searchPage_header">
-                <Link to="/">
+                <Link to='/'>
                     <img 
                       className="searchPage_logo"
                       src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
@@ -70,19 +73,27 @@ function SearchPage() {
             {term && (
                 <div className="searchPage_results">
                     <p className="searchPage_resultsCount"> About {data?.searchInformation.formattedTotalResults}
-                      <span> </span>({data?.searchInformation.formattedSearchTime} seconds)
+                      <span> results </span>({data?.searchInformation.formattedSearchTime} seconds)
                     </p>
                     { data?.items.map( item => (
                         <div className="searchPage_result">
-                            <a href={item.link}> 
-                                {item.pagemap?.cse_image?.length > 0 && item.pagemap.cse_image[0].src && 
+                            <a className="searchPage_resultLink" href={item.link}> 
+                                {(item.pagemap?.videoobject && (
+                                    <iframe width="420" height="315"
+                                    src={item.pagemap.videoobject[0].embedurl}>
+                                    </iframe>
+                                )) ||
+                                (item.pagemap?.cse_image?.length > 0 && item.pagemap.cse_image[0].src && 
                                 (
+                                <div className="searchPage_resultTop">
                                     <img className="searchPage_resultImage"
                                      src={item.pagemap.cse_image[0].src} 
                                      alt=""
-                                    />
-                                ) }
-                                {item.displayLink} 
+                                    />  
+                                    <span>{item.displayLink}</span> <ArrowDropDownIcon />
+                                </div>
+                                ))
+                                }  
                             </a>
                             <a className="searchPage_resultTitle" href={item.link}>
                                 <h2>{item.title}</h2>
